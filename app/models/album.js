8ea90b05062751fbe7dff6914c6df7bdf6a9c3af
Steve Kinney <hello@stevekinney.net>
2016-01-20 14:17:42 -0700

    Add models

---
+++ app/models/album.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  year: DS.attr('string'),
+  artist: DS.belongsTo('artist'),
+  songs: DS.hasMany('song')
+});
