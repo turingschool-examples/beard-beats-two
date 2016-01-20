d215053c987839191465bc16d2950964119da97c
Steve Kinney <hello@stevekinney.net>
2016-01-20 15:47:00 -0700

    Add album and song template

--- app/router.js
+++ app/router.js
@@ -7,7 +7,12 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('artists', function() {
-    this.route('artist', { path: ':artist_id' });
+    this.route('artist', { path: ':artist_id' }, function() {
+      this.route('albums', function() {
+        this.route('album', { path: ':album_id' }, function() {
+        })
+      })
+    });
   });
 });
 
